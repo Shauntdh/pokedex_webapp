12617c727b9e111967219c8dccc900bd99ecea47
Shaunt Der Haroutunian <shauntdh@gmail.com>
2022-06-26 16:22:33 -0700

    added and configured trpc

---
+++ src/pages/api/trpc/[trpc].ts
@@ -0,0 +1,9 @@
+// export API handler
+
+import { appRouter } from "../../../server/routers/app";
+import * as trpcNext from "@trpc/server/adapters/next";
+
+export default trpcNext.createNextApiHandler({
+  router: appRouter,
+  createContext: () => null,
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from "next";
-import Searchbar from "../components/Searchbar";
-import PokemonLogo from "../public/svg/Pokemon.svg";
+import { trpc } from "../utils/trpc";
+import Searchbar from "../../components/Searchbar";
+import PokemonLogo from "../../public/svg/Pokemon.svg";
 
 const Home: NextPage = () => {
+  const { data, isLoading } = trpc.useQuery(["hello", { text: "Shaunt" }]);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (data) return <div>{data.greeting}</div>;
+
   return (
     <main className="my-0 mx-auto w-full max-w-8xl min-h-screen py-0 px-60">
       <section className="flex flex-col justify-center items-center p-0 min-h-screen max-w-5xl -my-20">
